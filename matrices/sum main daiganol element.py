fc2b21f1fa5d7ffe006e6b2da695b25dc3e0dea6
vithyasri <52270841+vithyasri@users.noreply.github.com>
2020-01-08 15:13:01 +0530

    Create sum main daiganol element.py

---
+++ matrices/sum main daiganol element.py
@@ -0,0 +1,19 @@
+def printDiagonalSums(mat, n): 
+ 
+    principal = 0
+    secondary = 0; 
+    for i in range(0, n):  
+        for j in range(0, n):  
+            if (i == j): 
+                principal += mat[i][j] 
+            if ((i + j) == (n - 1)): 
+                secondary += mat[i][j] 
+ 
+    print("Principal Diagonal:", principal) 
+    print("Secondary Diagonal:", secondary) 
+ 
+a = [[ 1, 2, 3, 4 ], 
+     [ 5, 6, 7, 8 ],  
+     [ 1, 2, 3, 4 ], 
+      [ 5, 6, 7, 8 ]] 
+printDiagonalSums(a, 4)
